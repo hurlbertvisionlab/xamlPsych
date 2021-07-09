d0ef2f8e156dfb173024b8525b661ea63f6898fe
jantheman <jantheman@users.noreply.github.com>
2021-07-09 23:14:50 +0100

    break and if support

---
+++ XamlPsychHost/StudyObjects/StudySteps/Flow/When.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace HurlbertVisionLab.XamlPsychHost
+{
+    public class When : StudyStepCollection
+    {
+        public static readonly DependencyProperty StoreKeyProperty = DependencyProperty.Register(nameof(StoreKey), typeof(string), typeof(When));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(object), typeof(When));
+
+        public string StoreKey
+        {
+            get { return (string)GetValue(StoreKeyProperty); }
+            set { SetValue(StoreKeyProperty, value); }
+        }
+
+        public object Value
+        {
+            get { return (object)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        protected override Task Execute(CancellationToken cancellationToken)
+        {
+            StudyContext.Store.TryGetValue(StoreKey, out object value);
+
+            if (object.Equals(value, Value))
+                return base.Execute(cancellationToken);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ XamlPsychHost/StudyObjects/StudySteps/Iterators/BreakOpportunity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace HurlbertVisionLab.XamlPsychHost
+{
+    [ContentProperty(nameof(Steps))]
+    public class BreakOpportunity : StudyIterator
+    {
+        public static readonly DependencyProperty BreakGroupProperty = DependencyProperty.Register(nameof(BreakGroup), typeof(string), typeof(BreakOpportunity));
+        public static readonly DependencyProperty AfterProperty = DependencyProperty.Register(nameof(After), typeof(TimeSpan), typeof(BreakOpportunity));
+        public static readonly DependencyProperty EveryHitCountProperty = DependencyProperty.Register(nameof(EveryHitCount), typeof(int), typeof(BreakOpportunity));
+
+        public int EveryHitCount
+        {
+            get { return (int)GetValue(EveryHitCountProperty); }
+            set { SetValue(EveryHitCountProperty, value); }
+        }
+
+        public string BreakGroup
+        {
+            get { return (string)GetValue(BreakGroupProperty); }
+            set { SetValue(BreakGroupProperty, value); }
+        }
+
+        public TimeSpan After
+        {
+            get { return (TimeSpan)GetValue(AfterProperty); }
+            set { SetValue(AfterProperty, value); }
+        }
+
+        protected override async Task Execute(CancellationToken cancellationToken)
+        {
+            DateTimeOffset studyStarted = StudyContext.Started.GetValueOrDefault();
+            bool breakOpportunity = true;
+
+            if (After >= TimeSpan.Zero)
+            {
+                DateTimeOffset lastBreakEnded = studyStarted;
+                if (StudyContext.Store.TryGetValue("LastBreakEnded:" + BreakGroup, out object value) && value is DateTimeOffset timestamp)
+                    lastBreakEnded = timestamp;
+
+                TimeSpan sinceLastBreak = DateTimeOffset.Now - lastBreakEnded;
+                breakOpportunity = sinceLastBreak >= After;
+                
+                StudyContext.Log(this, this, nameof(After), breakOpportunity, sinceLastBreak);
+            }
+
+            if (EveryHitCount > 0)
+            {
+                int hitCount = 0;
+                if (StudyContext.Store.TryGetValue("BreakHitCount:" + BreakGroup, out object value) && value is int count)
+                    hitCount = count;
+
+                StudyContext.Store["BreakHitCount:" + BreakGroup] = ++hitCount;
+
+                int modulo = hitCount % EveryHitCount;
+                breakOpportunity = modulo == 0;
+
+                StudyContext.Log(this, this, nameof(EveryHitCount), breakOpportunity, hitCount);
+            }
+
+            if (breakOpportunity)
+            {
+                DateTimeOffset breakStarted = DateTimeOffset.Now;
+                StudyContext.Log(this, this, "BreakStart", BreakGroup, breakStarted - studyStarted);
+                await StudyContext.Execute(Steps, ItemContext, cancellationToken);
+                
+                DateTimeOffset breakEnded = DateTimeOffset.Now;
+                StudyContext.Log(this, this, "BreakEnd", BreakGroup, breakEnded - breakStarted);
+                
+                StudyContext.Store["LastBreakEnded:" + BreakGroup] = breakEnded;
+                StudyContext.Store["LastBreakEnded:"] = breakEnded; // any break
+            }
+        }
+    }
+
+    public class ResetBreakOpportunity : StudyStep
+    {
+        public static readonly DependencyProperty BreakGroupProperty = DependencyProperty.Register(nameof(BreakGroup), typeof(string), typeof(ResetBreakOpportunity));
+
+        public string BreakGroup
+        {
+            get { return (string)GetValue(BreakGroupProperty); }
+            set { SetValue(BreakGroupProperty, value); }
+        }
+
+        protected override Task Execute(CancellationToken cancellationToken)
+        {
+            StudyContext.Store["LastBreakEnded:" + BreakGroup] = DateTimeOffset.Now;
+            StudyContext.Store["BreakHitCount:" + BreakGroup] = 0;
+            return Task.CompletedTask;
+        }
+    }
+}
