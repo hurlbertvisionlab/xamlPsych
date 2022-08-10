937540194585f07418209fe0c2b863c539ed302a
Jan Kučera <v-jankucera@microsoft.com>
2022-08-10 10:47:13 +0100

    remaining time control supporting progress bar

---
+++ XamlPsychHost/Controls/RemainingTime.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace HurlbertVisionLab.XamlPsychHost
+{
+    public enum RemainingDisplay
+    {
+        None,
+        Text,
+        Progress,
+        ProgressIncreasing
+    }
+
+    public partial class RemainingTime : UserControl
+    {
+        public RemainingTime()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- XamlPsychHost/StudyObjects/StudySteps/StudyUIStep.cs
+++ XamlPsychHost/StudyObjects/StudySteps/StudyUIStep.cs
@@ -5,6 +5,14 @@ namespace HurlbertVisionLab.XamlPsychHost
 {
     public abstract class StudyUIStep : StudyStep
     {
+        public static readonly DependencyProperty RemainingToAdvanceDisplayProperty = DependencyProperty.Register(nameof(RemainingToAdvanceDisplay), typeof(RemainingDisplay), typeof(StudyUIStep), new PropertyMetadata(RemainingDisplay.Text));
+
+        public RemainingDisplay RemainingToAdvanceDisplay
+        {
+            get { return (RemainingDisplay)GetValue(RemainingToAdvanceDisplayProperty); }
+            set { SetValue(RemainingToAdvanceDisplayProperty, value); }
+        }
+
         protected static object CoerceText(DependencyObject d, object baseValue)
         {
             if (baseValue is string s)
