16bade754223f43d7129f6c1e6089d546763d711
jantheman <jantheman@users.noreply.github.com>
2023-05-09 16:03:42 +0100

    support arbitrary objects in AskForStep.Prompt

--- XamlPsychHost/StudyObjects/StudySteps/Input/AskForStep.cs
+++ XamlPsychHost/StudyObjects/StudySteps/Input/AskForStep.cs
@@ -1,13 +1,16 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace HurlbertVisionLab.XamlPsychHost
 {
     public class AskForStep : StudyUIStep
     {
-        public static readonly DependencyProperty PromptProperty = DependencyProperty.Register(nameof(Prompt), typeof(string), typeof(AskForStep), new PropertyMetadata(null));
-        public static readonly DependencyProperty ContinueTextProperty = DependencyProperty.Register(nameof(ContinueText), typeof(string), typeof(AskForStep), new PropertyMetadata("Continue"));
+        public static readonly DependencyProperty PromptProperty = DependencyProperty.Register(nameof(Prompt), typeof(object), typeof(AskForStep), new PropertyMetadata(null, null, CoerceText));
+        public static readonly DependencyProperty ContinueTextProperty = DependencyProperty.Register(nameof(ContinueText), typeof(string), typeof(AskForStep), new PropertyMetadata("Continue", null, CoerceText));
 
         public string ContinueText
         {
@@ -15,10 +18,32 @@ namespace HurlbertVisionLab.XamlPsychHost
             set { SetValue(ContinueTextProperty, value); }
         }
 
-        public string Prompt
+        public object Prompt
         {
-            get { return (string)GetValue(PromptProperty); }
+            get { return GetValue(PromptProperty); }
             set { SetValue(PromptProperty, value); }
+        }
+
+        private string GetTextForLog(object o)
+        {
+            if (o == null)
+                return null;
+
+            if (o is string s) return s;
+            if (o is ILogInfo info) return info.ToLogString(StudyContext);
+            if (o is TextBlock tb) return tb.Text;
+            if (o is TextBox tbx) return tbx.Text;
+            if (o is RichTextBox rtb) return rtb.Document.ToString();
+            if (o is Label lb) return GetTextForLog(lb.Content);
+            if (o is Image { Source: BitmapImage i }) return i.UriSource?.ToString();
+            if (o is DependencyObject d)
+            {
+                foreach (var child in LogicalTreeHelper.GetChildren(d))
+                    if (GetTextForLog(child) is string childText)
+                        return childText;
+            }
+
+            return o.ToString();
         }
 
         protected static void LogAndStoreAsResult(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -37,7 +62,7 @@ namespace HurlbertVisionLab.XamlPsychHost
 
         protected override Task Execute(CancellationToken cancellationToken)
         {
-            StudyContext.Log(this, this, "Prompt", Prompt);
+            StudyContext.Log(this, this, "Prompt", GetTextForLog(Prompt));
             StudyContext.Screen.Show(this);
             return Task.CompletedTask;
         }
