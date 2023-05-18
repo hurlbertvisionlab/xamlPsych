945ed9c9a80d57443546739a3e895c7881f892d2
jantheman <jantheman@users.noreply.github.com>
2023-05-18 15:48:58 +0100

    starting value for AskForText

--- XamlPsychHost/StudyObjects/StudySteps/Input/AskForTextInput.cs
+++ XamlPsychHost/StudyObjects/StudySteps/Input/AskForTextInput.cs
@@ -8,8 +8,14 @@ namespace HurlbertVisionLab.XamlPsychHost
     [ContentProperty(nameof(ValidationRules))]
     public class AskForTextInput : AskForStep
     {
+        public static readonly DependencyProperty StartingInputProperty = DependencyProperty.Register(nameof(StartingInput), typeof(string), typeof(AskForTextInput));
         public static readonly DependencyProperty InputProperty = DependencyProperty.Register(nameof(Input), typeof(string), typeof(AskForTextInput), new PropertyMetadata(null, LogAndStoreAsResult));
 
+        public string StartingInput
+        {
+            get { return (string)GetValue(StartingInputProperty); }
+            set { SetValue(StartingInputProperty, value); }
+        }
         public string Input
         {
             get { return (string)GetValue(InputProperty); }
@@ -18,5 +24,12 @@ namespace HurlbertVisionLab.XamlPsychHost
 
         public Collection<ValidationRule> ValidationRules { get; } = new Collection<ValidationRule>();
         public System.Collections.ICollection ValidationErrors { get; } = new object[0];
+
+        protected override void PrepareForNewExecution()
+        {
+            Input = StartingInput;
+            base.PrepareForNewExecution();
+        }
+
     }
 }
