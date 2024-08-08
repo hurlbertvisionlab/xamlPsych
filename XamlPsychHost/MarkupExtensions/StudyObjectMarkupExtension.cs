32232baccb77a52b7b7e8abdeeab6fb500bf0fab
jantheman <jantheman@users.noreply.github.com>
2024-08-08 17:56:48 +0100

    Support markup extensions in templates

--- XamlPsychHost/MarkupExtensions/StudyObjectMarkupExtension.cs
+++ XamlPsychHost/MarkupExtensions/StudyObjectMarkupExtension.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Markup;
 
 namespace HurlbertVisionLab.XamlPsychHost
 {
     public abstract class StudyObjectMarkupExtension : MarkupExtension
     {
+        private class Duplicate : StudyObjectMarkupExtension
+        {
+            public Duplicate(StudyObjectMarkupExtension clone) : base(clone.Prefix, clone.Path) { }
+        }
+
         public string Path { get; set;  }
         protected string Prefix { get; }
 
@@ -15,22 +21,35 @@ namespace HurlbertVisionLab.XamlPsychHost
             Prefix = property.Name;
             Path = path;
         }
+        private StudyObjectMarkupExtension(string prefix, string path)
+        {
+            Prefix = prefix;
+            Path = path;
+        }
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return CreateBinding(serviceProvider).ProvideValue(serviceProvider);
+            IProvideValueTarget target = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+
+            if (target.TargetObject?.GetType().FullName == "System.Windows.SharedDp")
+                return new Duplicate(this);
+
+            return CreateBinding(target).ProvideValue(serviceProvider);
         }
 
         internal Binding CreateBinding(IServiceProvider serviceProvider)
         {
             IProvideValueTarget target = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
-
+            return CreateBinding(target);
+        }
+        internal Binding CreateBinding(IProvideValueTarget target)
+        {
             RelativeSource source;
             if (target.TargetObject is StudyObject)
                 source = new RelativeSource(RelativeSourceMode.Self);
             else
                 source = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(StudyStep), 1);
-            
+
             string path = Prefix;
             if (!string.IsNullOrEmpty(Path) && !Path.StartsWith("#"))
             {
@@ -40,7 +59,9 @@ namespace HurlbertVisionLab.XamlPsychHost
                     path += "." + Path;
             }
 
-            return new Binding(path) { RelativeSource = source };
+            // TODO: we should deal with the target not being in the tree yet (i.e. a template is being instantiated, target.Parent = null and RelativeSource fails)
+            // however in the most common case, ShowStimuli, the templated object is a StudyStep itself and we can rely on its DataContext
+            return new Binding(path) { RelativeSource = this is Duplicate ? null : source };
         }
     }
 
