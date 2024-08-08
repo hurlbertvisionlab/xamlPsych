80319577437b4eb1d926387fc106fa22e192e44b
jantheman <jantheman@users.noreply.github.com>
2024-08-08 19:01:40 +0100

    TimeStamp, TimeCheck

---
+++ XamlPsychHost/StudyObjects/StudySteps/Timers/TimeCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HurlbertVisionLab.XamlPsychHost
+{
+    public class TimeCheck : StudyStep
+    {
+        public static readonly DependencyProperty StampIDProperty = DependencyProperty.Register(nameof(StampID), typeof(string), typeof(TimeCheck));
+
+        public string StampID
+        {
+            get { return (string)GetValue(StampIDProperty); }
+            set { SetValue(StampIDProperty, value); }
+        }
+
+        protected override Task Execute(CancellationToken cancellationToken)
+        {
+            if (StudyContext.Store.TryGetValue("Timer:" + StampID, out var value) && value is DateTimeOffset start)
+                Result = DateTimeOffset.Now - start;
+            else
+                Result = "N/A";
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ XamlPsychHost/StudyObjects/StudySteps/Timers/TimeStamp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HurlbertVisionLab.XamlPsychHost
+{
+    public class TimeStamp : StudyStep
+    {
+        protected override Task Execute(CancellationToken cancellationToken)
+        {
+            StudyContext.Store["Timer:" + ID] = DateTimeOffset.Now;
+            
+            return Task.CompletedTask;
+        }
+    }
+}
